122babc857299b8bcb7601b1d214dc973ad6184a
Grant Register <register.grant@gmail.com>
2022-06-20 21:33:17 -0600

    added some complicated javascript

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const currentTempElement = document.getElementById("current-temp");
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
+const API_KEY = '74fec2d8057ec0ff885d19b7ff4a5faa';
+
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
@@ -21,3 +23,18 @@ setInterval(() => {
     timeElement.innerHTML = hoursIn12HourFormat + ':' + minutes + ' ' + `<span id="am-pm">${ampm}</span>`
     dateElement.innerHTML = days[day] + ', ' + date + ' ' + months[month];
 }, 1000);
+
+getWeatherData();
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+      
+        let {latitude, longitude} = success.coords;
+
+        // Getting a 401 unauthorized api key message here
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=
+        ${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=
+        ${API_KEY}`).then(res => res.json()).then(data => {
+            console.log(data)
+        })
+    })
+}
