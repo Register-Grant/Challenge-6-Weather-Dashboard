c818f0c9f16f52deb13f62bc987ed04bed9f283b
Grant Register <register.grant@gmail.com>
2022-06-21 21:10:07 -0600

    somehow one single space was breaking the javascript

--- scripttaketwo.js
+++ scripttaketwo.js
@@ -1,3 +1,16 @@
 let weather = {
-    "apiKey": "74fec2d8057ec0ff885d19b7ff4a5faa"
-}
+    apiKey: "74fec2d8057ec0ff885d19b7ff4a5faa",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" 
+            + city 
+            + "&units=imperial&appid=" 
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+    displayWeather: function(data) {
+
+    }
+};
