ba99c51e81de4d079892228a9a87444d368ea555
Grant Register <register.grant@gmail.com>
2022-06-23 20:32:43 -0600

    got it working for one city, but no 5 day forecast or local storage or uv index

--- scripttaketwo.js
+++ scripttaketwo.js
@@ -13,10 +13,33 @@ let weather = {
     displayWeather: function(data) {
         const {name} = data;
         const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
+        const {temp, humidity,} = data.main;
         const {speed} = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = 
+        "Humidity: " + humidity + "%";
+        document.querySelector(".wind-speed").innerText = 
+        "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
+
+document.querySelector(".search button")
+.addEventListener("click", function () {
+weather.search();
+});
+
+document.querySelector(".search-bar")
+.addEventListener("keyup", function (event) {
+if (event.key == "Enter") {
+    weather.search();
+}
+});
+
+weather.fetchWeather();
