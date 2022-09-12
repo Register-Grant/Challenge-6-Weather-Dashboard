30da5f15113dc7c76bc4f806bc2a9d64b883cb57
Grant Register <register.grant@gmail.com>
2022-09-12 13:45:48 -0600

    got the bugs out

--- scripttakethree.js
+++ scripttakethree.js
@@ -1,23 +1,25 @@
 function GetInfo() {
-    const newName = document.getElementById("cityInput");
-    const cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--"
+    let newName = document.getElementById("cityInput");
+    let cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=3e282ef0bced111943e6341c3fed62c8")
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=3e282ef0bced111943e6341c3fed62c8')
 .then(response => response.json())
 .then(data => {
-    for(i=0;i<5;i++) {
-        document.getElementById("day"+(i+1)+"Temp").innerHTML = "Temp:" + Number(data.list[i].main.temp -304.56).toFixed(1)+"°";
+    for(i=0; i<5; i++) {
+        document.getElementById("day" + (i+1) + "Temp").innerHTML = "Temp " + Number(data.list[i].main.temp).toFixed(1)+"°";
     }
-    for(i=0;i<5;i++) {
-        document.getElementById("day"+(i+1)+"Wind").innerHTML = "Wind Speed:" + Number(data.list[i].wind.speed -5.87).toFixed(1)+"km/h";
+    for(i=0; i<5; i++) {
+        document.getElementById("day" + (i+1) + "Wind").innerHTML = "Wind Speed " + Number(data.list[i].wind.speed).toFixed(1)+"km/h";
     }
-    for(i=0;i<5;i++) {
-        document.getElementById("day"+(i+1)+"Humidity").innerHTML = "Humidity:" + Number(data.list[i].main.humidity)+"%";
+    for(i=0; i<5; i++) {
+        document.getElementById("day" + (i+1) + "Humidity").innerHTML = "Humidity " + Number(data.list[i].main.humidity)+"%";
     }
-    for(i=0;i<5;i++) {
-        document.getElementById("img" + (i+1)).src = " http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
+
+    for(i=0; i<5; i++) {
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
         }
+        console.log(data)
 })
 
 .catch(err => alert("You suck at this"))
@@ -28,8 +30,8 @@ function DefaultScreen() {
     GetInfo();
 }
 
-const d = new Date();
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let d = new Date();
+let weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 function CheckDay(day) {
     if(day + d.getDay() > 6) {
@@ -39,6 +41,6 @@ function CheckDay(day) {
     }
 }
 
-for(i=0;i<5;i++) {
-    document.getElementById("day"+(i+1)).innerHTML = weekday[CheckDay(i)];
+for(i=0; i<5; i++) {
+    document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
 }
